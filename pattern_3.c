d9719c890d5045eb781981613a5150f8e83d870c
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2023-07-23 21:25:53 +0530

    pattern 3

---
+++ pattern_3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,rows,cols;
+printf("Etner a number :");
+scanf("%d",&n);
+for(rows=1;rows<=n;rows++)
+{
+for(cols=1;cols<=rows;cols++)
+{
+printf("%d ",rows);
+}
+printf("\n");
+}
+
+return 0;
+}
